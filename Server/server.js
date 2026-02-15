79c8d8fed1ba66475003a88212f95e7c684d5860
kritimathur942 <kritimathur942@gmail.com>
2026-02-15 20:10:55 +0530

    updated server  file2

--- Server/server.js
+++ Server/server.js
@@ -8,15 +8,13 @@ import { inngest, functions } from "./inngest/index.js"
 const app = express();
 
 app.use(express.json());
-app.use(cors())
+app.use(cors());
 app.use(clerkMiddleware());
 
-app.get("/", (req, res)=> {res.send('Server is live!')});
+app.get("/", (req, res) => {
+  res.send("Server is live!");
+});
+
 app.use("/api/inngest", serve({ client: inngest, functions }));
 
-
-const PORT = process.env.PORT || 9000
-
-app.listen(PORT, ()=> console.log(`Server is running on port ${PORT}`))
-
-
+export default app;
